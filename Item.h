70adf643efb90e52c6c110711da82c8415e0be3a
RaziqinHusna <136729643+RaziqinHusna@users.noreply.github.com>
2023-06-26 00:55:45 +0800

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,43 @@
+#include<iostream>
+#include
+
+using namespace std;
+
+class Item
+{
+	private:
+		//coin item
+		struct Coin
+		{
+			int cVal;
+			Coin *nextCoin;
+		};
+		Coin *coinTop;
+		//business card item
+		struct Card
+		{
+			int bVal;
+			Card *nextCard;
+		};
+		Card *cardTop;
+		//food item
+		struct Food
+		{
+			int fVal;
+			Food *nextFood;
+		};
+		Food *foodTop;
+		
+		int count;
+		
+	public:
+		Item();
+		~Item();
+		void PushCoin(int);
+		void PushCard(int);
+		void PushFood(int);
+		void PopCoin(int);
+		void PopFood(int);
+		void PopCard(int);
+		bool isEmpty();
+};
