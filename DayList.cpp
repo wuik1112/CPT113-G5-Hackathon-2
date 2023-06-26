aebd60573e2b44b216c5bcacc5db5708e373af37
jx1114 <121542232+jx1114@users.noreply.github.com>
2023-06-26 10:52:30 +0800

    Update DayList.cpp

--- DayList.cpp
+++ DayList.cpp
@@ -1,1 +1,27 @@
+ // Add a day to the list
+    void addDay(DayPointer day) {
+        DayNode* newNode = new DayNode;
+        newNode->day = day;
+        newNode->next = nullptr;
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Invoke all days in the list
+    void invokeAllDays(Script& script, int d) {
+        DayNode* current = head;
+        for (int i=1; i<d; i++) {
+          current = current->next;
+        }
+        while (current != nullptr) {
+            (script.*(current->day))();
+            current = current->next;
+        }
+    }
 
