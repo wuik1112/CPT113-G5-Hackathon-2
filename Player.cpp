6fd21af8f86cf7d15461dd8eff19da9592081e60
RaziqinHusna <136729643+RaziqinHusna@users.noreply.github.com>
2023-06-26 12:26:47 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -7,50 +7,14 @@ Player::Player()
 {
 	//set initial state
 	name = "";
-	headHappy=nullptr;
-	countHappy = 0;
-
-	//initial 3 lives to player
-	head->lVal = 1;
-	head->next->lVal = 1;
-	head->next->next->lVal = 1;
 	countLives = 3;
+	countHappy = 0;
 }
 Player::~Player()
 {
-	//transverse and point to next node of player's lives
-	Lives *livePtr;
-	Lives *nextLive;
-	
-	//transverse and point to next node of player's happiness
-	Happiness *happyPtr;
-	Happiness *nextHappy;
-	
-	//position pointer at head
-	livePtr = headLives;
-	happyPtr = headHappy;
-	
-	//while pointer not at the end of the list, delete node.
-	//lives
-	while(livePtr != nullptr)
-	{
-		//save pointer to next node
-		nextLive = livePtr->nextLives;
-		//delete current node
-		delete livePtr;
-		//position livePtr at next node
-		livePtr = livePtr->nextLives;
-	}
-	//happiness
-  while(happyPtr != nullptr)
-	{
-		//save pointer to next node
-		nextHappy = happyPtr->nextHappy;
-		//delete current node
-    delete happyPtr;
-		//position happyPtr at next node
-		happyPtr = nextHappy;
-	}
+	name = "";
+	countLives = 0;
+	countHappy = 0;
 }
 void Player::getName(string n)
 {
@@ -60,22 +24,14 @@ string Player::setName()
 {
 	return name;
 }
-int Player::getLives()
-{
-	
-}
 void Player::deleteLives()
 {
-	Lives *livePtr;
-	
-	livePtr = headLives;
-	while(livePtr->nextLives != nullptr)
-	{
-		livePtr = livePtr->nextLives;
-	}
-	//delete the last node
-	delete livePtr;
+	//lose lives
 	countLives--;
+}
+int Player::getLives()
+{
+	return countLives;
 }
 /*bool Player::checkPlayerStatus()
 {
@@ -85,39 +41,13 @@ void Player::deleteLives()
 		return false;
 	}
 }*/
-int Player::getHappiness()
-{
-	
-}
 void Player::appendHappiness(int num)
 {
-	Happiness *newHappy; //point new Happiness
-	Happiness *happyPtr; //move through list
-	
-	//allocate new Happiness and store num
-	newHappy = new Happiness;
-	newHappy->hVal = num;
-	newHappy->nextHappy = nullptr;
-	
-	//if no node yet in the list
-	if(!headHappy)
-	{
-		headHappy = newHappy;
-	}
-	else
-	{
-		//initialise happyPtr to head of list
-		happyPtr = headHappy;
-		
-		//find last happiness
-		while (happyPtr->nextHappy)
-		{
-			happyPtr = happyPtr->nextHappy;
-		}
-		
-		happyPtr->nextHappy = newHappy;
-		countHappy++;
-	}
+	countHappy++;
+}
+int Player::getHappiness()
+{
+	return countHappiness;
 }
 /*void Player::happinessCount()
 {
