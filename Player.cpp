04b40cd02b192b1b8ff90ddbebbe27cd577ab006
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 21:08:19 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include "Player.h"
 #include "Game.h"
 
 using namespace std;
 
+// Constructor to initialize value of player
 Player::Player()
 {
 	//set initial state
@@ -12,47 +13,42 @@ Player::Player()
 	countLives = 3;
 	countHappiness = 0;
 }
+// Destructor to return all value to 0
 Player::~Player()
 {
 	name = "";
 	countLives = 0;
 	countHappiness = 0;
 }
+
+// hold the player name after they input the name
 void Player::setName(string n)
 {
 	name = n;
 }
+// return the inputted value that stored in name
 string Player::getName()
 {
 	return name;
 }
+
+// hold the number of lives left of the cat
 void Player::setLives(int lives)
 {
 	countLives = lives;
 }
+// return the number of lives that stores in countLives
+int Player::getLives()
+{
+	return countLives;
+}
+// decrease number of lives when player make choice to decrease lives
 void Player::deleteLives(int num)
 {
 	//lose lives
 	countLives = countLives - num;
 }
-int Player::getLives()
-{
-	return countLives;
-}
-void Player::setHappiness(int happy)
-{
-	countHappiness = happy;
-}
-void Player::addHappiness(int num)
-{
-	countHappiness = countHappiness + num;
-}
-int Player::getHappiness()
-{
-	return countHappiness;
-}
-
-// call if player lives less than and equal to 0
+// call if number of lives less than and equal to 0
 void Player::checkLives()
 {
 	if (countLives <= 0)
@@ -61,4 +57,19 @@ void Player::checkLives()
 		cout << "Your journey ends here...Game Over. "<<endl;
 		game->quitGame();
 	}
+}
+
+// hold the happiness value of cat
+void Player::setHappiness(int happy)
+{
+	countHappiness = happy;
+}
+
+int Player::getHappiness()
+{
+	return countHappiness;
+}
+void Player::addHappiness(int num)
+{
+	countHappiness = countHappiness + num;
 }
