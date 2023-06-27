cbdd175825ed91af720d6cfb1cdc671f30f82e7a
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 20:47:30 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -11,22 +11,30 @@ class Game; // forward declaration
 class Player
 {
 	private:
-    	Game* game;
-		string name;
-		int countLives;
-		int countHappiness;
+    		Game* game; // to use function in game class
+		string name; // variable to store player's name
+		int countLives; // variable to store the current number of lives
+		int countHappiness; // variable to store the current happiness
 	public:
-		Player();
-    	~Player();
-   	 	string getName();
+		// Constructor
+		Player(); 
+		// Destructor
+    		~Player();
+		// to hold the name inputted by player
   	  	void setName(string);
+		string getName();
+		// to hold the lives of cat in the game
 		void setLives(int);
 		int getLives();
+		// decrease lives when player make choice to decrease lives
 		void deleteLives(int);
+		// check if cat used up the lives, if yes, game over
+		void checkLives();
+		// to hold the value of happiness of cat
 		void setHappiness(int);
 		int getHappiness();
+		// to increase the happiness if player make choice that make cat happy
 		void addHappiness(int);
-		void checkLives();
 };
 
 #endif
