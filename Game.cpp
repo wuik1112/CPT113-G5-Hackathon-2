102b5f4cf9783d69f52f274abbe9bf269946eeeb
jx1114 <121542232+jx1114@users.noreply.github.com>
2023-06-27 18:41:19 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <fstream> // multiple file inclusion
-#include <string> 
+#include <string>
 #include "Game.h"
 #include "Player.h"
 #include "Script.h"
+#include "Item.h"
 
 //Constructor
 Game::Game()
 {
-	
+
 }
 
 Game::~Game()
 {
-	
+
 }
 
 //Start a new game
 void Game::startGame()
 {
 	string name;
-	
+
 	cout << "Welcome to our game, \"Feline: Far From Home\". " << endl;
-	getchar(); 
+	getchar();
 	cout << "This is a story about a kitten lost its owner." << endl
 	     << "You need to make decision for each condition to help the kitten reunites with its owner. " << endl;
 
-	cout << "Please give a name for this kitten: " << endl; 
+	cout << "Please give a name for this kitten: ";
 	getline(cin, name);
 	player.setName(name);
 
@@ -35,34 +36,36 @@ void Game::startGame()
 	cout << "Your Lives: " << player.getLives() << endl;
 	cout << "Your Happiness: " << player.getHappiness() << endl;
 	getchar();
-	cout << "Enjoy your journey! " << endl;
+	cout << "Enjoy your journey! " << endl << endl;
+
+	saveProgress();
 
 	script.day1();
 }
 
 
 //Save progress to text file
-void Game::saveProgress(script.getCurrentChapter())
+void Game::saveProgress()
 {
 	// open the text file to write in
-    ofstream outputFile("progress.txt");
+	ofstream outFile("progress.txt");
 
-	if (outputFile.is_open()) {
+	if (outFile.is_open()) {
 		// write the player info into the file
-		outputFile << player.getName() << endl;
-		outputFile << player.getLives() << endl;
-		outputFile << player.getHappiness() << endl;
+		outFile << player.getName() << endl;
+		outFile << player.getLives() << endl;
+		outFile << player.getHappiness() << endl;
 		// write the items hold by player into the file
-		outputFile << item.getCoinCount() << endl;
-		outputFile << item.getFoodCount() << endl;
-		outputFile << item.getCardCount() << endl; 
+		outFile << item.getCoinCount() << endl;
+		outFile << item.getFoodCount() << endl;
+		outFile << item.getCardCount() << endl;
 		// write the current chapter to text file
-        	outputFile << script.getCurrentChapter() << endl; 
-		
-		outputFile.close();  // Close the text file
-        
+    	outFile << script.getCurrentChapter() << endl;
+
+		outFile.close();  // Close the text file
+
 		cout << endl << "Progress saved successfully! " << endl;
-    
+
 	} else {
         cout << endl << "Failed to save the progress." << endl;
     }
@@ -72,19 +75,19 @@ void Game::saveProgress(script.getCurrentChapter())
 void Game::loadProgress()
 {
 	ifstream inFile("progress.txt");
-	
+
 	if (inFile.is_open())
 	{
-        	string data; 
+        	string data;
 		int num;
 		getline(inFile, data);
 		player.setName(data);
 		int i = 1;
-		while (cin >> num) 
+		while (inFile >> num)
 		{
 			switch (i)
 			{
-				case 1: 
+				case 1:
 				{
 					player.setLives(num);
 					break;
@@ -94,25 +97,25 @@ void Game::loadProgress()
 					player.setHappiness(num);
 					break;
 				}
-				case 3: 
+				case 3:
 				{
 					for (int i=0; i<num; i++)
 						item.PushCoin();
 					break;
 				}
-				case 4: 
+				case 4:
 				{
 					for (int i=0; i<num; i++)
 						item.PushFood();
 					break;
 				}
-				case 5: 
+				case 5:
 				{
 					for (int i=0; i<num; i++)
 						item.PushCard();
 					break;
 				}
-				case 6: 
+				case 6:
 				{
 					script.setCurrentChapter(num);
 					break;
@@ -129,39 +132,59 @@ void Game::loadProgress()
 	//according to the current chapter, display the current chapter
 	switch (script.getCurrentChapter())
 	{
-		case 1: day1();
-		case 2: day2();
-		case 3: day3();
-		case 4: day4();
-		case 5: day5();
+		case 1:
+		{
+			script.day1();
+			break;
+		}
+		case 2:
+		{
+			script.day2();
+			break;
+		}
+		case 3:
+		{
+			script.day3();
+			break;
+		}
+		case 4:
+		{
+			script.day4();
+			break;
+		}
+		case 5:
+		{
+			script.day5();
+			break;
+		}
 	}
-		
-	// // after display the current chapter, delete the content in text file to prevent future confusion
-	// deleteProgress();
+
+	 // after display the current chapter, delete the content in text file to prevent future confusion
+	 deleteProgress();
 }
 
-// void Game::deleteProgress()
-// {
-// 	// overwrite the content with empty
-// 	ofstream deleteFile("progress.txt", ofstream::out | ofstream::trunc);
+void Game::deleteProgress()
+{
+	// overwrite the content with empty
+	ofstream deleteFile("progress.txt", ofstream::out | ofstream::trunc);
 
-//     if (file.is_open()) 
-//     {
-//         file.close();
-//     	cout << "Progress deleted. " << endl;
-//     } else
-//     	cout << "Failed to delete the progress. " << endl;
-// }
+	if (deleteFile.is_open())
+     {
+        deleteFile.close();
+	    	cout << "Progress deleted. " << endl;
+     } else
+     		cout << "Failed to delete the progress. " << endl;
+ }
 
 // Ask player to continue or quit game
 void Game::continueGame()
 {
 	// variable that hold player's choice
 	int num = 0;
-	
+
 	// Ask if he would like to continue
 	cout << "Do you want to continue to next chapter?\n";
-	cout << "1. Yes\n2. No (Quit)\n";
+	cout << "1. Yes\n2. No (Back to main menu)\n";
 	cout << "Please enter your choice: ";
 	// player input
 	cin >> num;
@@ -174,13 +197,19 @@ void Game::continueGame()
 		cin >> num;
 	}
 
-	// save or exit program based on player choice
+	//save or exit program based on player choice
 	switch (num)
 	{
 		case 1:
-			saveProgress(script.getCurrentChapter());
-		case 2: 
-			quitGame();
+		{
+			saveProgress();
+			break;
+		}
+		case 2:
+		{
+			backToMainMenu();
+			break;
+		}
 	}
 }
 
@@ -189,8 +218,106 @@ void Game::quitGame()
 {
 	cout << "Quiting the game..." << endl;
 	// Automatically save the progress before quiting the game
-	saveProgress(script.getCurrentChapter());
+	saveProgress();
+	cout << "Game exited. " << endl;
 	// Exit the program
 	exit(0);
-	cout << "Game exited. " << endl;
-} 
+}
+
+// Back to main menu
+void Game::backToMainMenu()
+{
+	cout << "Welcome to...\n";
+	cout << "-----------------------------\n";
+	cout << "Feline: Far From Home\n";
+	cout << "-----------------------------\n";
+	cout << "1.Start\n2.Continue \n3.How To Play\n4.Credits\n5.Quit\n";
+	cout << "-----------------------------\n";
+	cout << "Please enter your choice: ";
+	cin >> num;
+
+	// player can choose 1 to 5
+	while(num < 1 || num > 5)
+	{
+		cout << "Enter only number from 1 to 5: ";
+		cin >> num;
+	}
+	switch(num)
+	{
+		case 1:
+		{
+			// call the function to start game
+			startGame();
+			break;
+		}
+		case 2:
+		{
+			int choice;
+			// confirmation of player's choice
+			cout << "Do you wish to continue your past progress?\n";
+			cout << "1.Yes\n2.No\n3.delete past progress\n";
+			cin >> num;
+
+			// input validation
+			while(num < 1 || num > 3)
+			{
+				cout << "Input number from 1 to 3: ";
+				cin >> num;
+			}
+
+			if(num == 1)
+				// choose to continue game and load previous progress
+				loadProgress();
+			else if(num == 2)
+			{
+				cout << "Returning to Main Menu...\n";
+				// function to return to main menu
+				backToMainMenu();
+			}
+			else
+			{
+				// delete the content in progress.txt
+				deleteProgress();
+			}
+
+			break;
+		}
+		case 3:
+		{
+			//how to play
+			break;
+		}
+		case 4:
+		{
+			// Credits of contributor of the game
+			cout << "Game Developers: \n";
+			cout << "Khoo Iu Wan\nLoo Jia Xin\nRaziqin Husna Binti Abdul Wahid\n";
+			cout << "Special Thanks to: \n";
+			cout << "Dr. Teh Je Sen\nDr Nur Hana Samsudin\nDr Siti Hazyanti Mohd Hashim\n";
+			cout << "All Gratitude Toward OYEN For Game Inspiration!!\n\n";
+			cout << "Do you want to back to main menu?\n";
+			cout << "1. Yes\n2. No\n";
+			cout << "Please enter your choice: ";
+			cin >> num;
+			while (num != 1 && num != 2){
+				cout << "Invalid choice.\n";
+				cout << "Please reenter your choice: ";
+				cin >> num;
+			}
+			if (num == 1){
+				// function to return to main menu
+				backToMainMenu();
+			}
+			else if (num == 2){
+				quitGame();
+			}
+			break;
+		}
+		case 5:
+		{
+			// call the function to quit game
+			quitGame();
+			break;
+		}
+	}
+}
