b1312586fb2886dc9927222decc9216dcf3dc17a
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 22:49:11 +0800

    Update Script.h

--- Script.h
+++ Script.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "Player.h"
 #include "Item.h"
+//#include "Game.h"
 
 using namespace std;
 
@@ -17,6 +18,7 @@ class Script
 		Player player;
 		Game* game;
 		Item item;
+		
 		int ending;
 		int currentChapter;
     int num;
@@ -26,25 +28,27 @@ class Script
 		Script();
 		// Destructor
 		~Script();
+		
 		void setEnding(int end);
 		int getEnding();
+		
 		// to record the chapter ongoing
 		void setCurrentChapter(int currentChapter);
 		int getCurrentChapter();
+		
 		// to display the corresponding story
 		void day1();
 		void day2();
 		void day3();
 		void day4();
 		void day5();
+		
 		// display the endings depends on player choice
 		void ending1();
 		void ending2();
 		void ending3();
 		// if happiness > X, this ending will happen
 		void secretEnding();
-		// call if player lives less than and equal to 0
-		void checkLives();
 };
 
 #endif
