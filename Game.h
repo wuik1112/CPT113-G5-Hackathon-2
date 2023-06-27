fe4edf37c44f962279ec14147ddaa53e9f8f5f74
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 22:51:41 +0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -19,26 +19,30 @@ class Game
 		Item item;
 		// to use function in Script class
 		Script script;
+		
 		int num;
-
 
 	public:
 		//Constructor
 		Game();
 		//Destructor
 		~Game();
+		
 		//Start a new game
 		void startGame();
+		
 		//Save progress to text file
 		void saveProgress();
 		//Load progress from text file
 		void loadProgress();
 		// Delete progress in text file
 		void deleteProgress();
+		
 		// Ask player if they wanna continue or quit game
 		void continueGame();
 		//Quit the game
 		void quitGame();
+		
 		// Back to main menu
 		void backToMainMenu();
 };
