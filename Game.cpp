467bba1e50a089f215f12099f4d26cd61ce47bd7
jx1114 <121542232+jx1114@users.noreply.github.com>
2023-06-27 21:28:09 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -285,6 +285,12 @@ void Game::backToMainMenu()
 		case 3:
 		{
 			//how to play
+			cout << "HOW TO PLAY\n";
+			cout << "------------------\n";
+			cout << "You are paying as a tabby orange cat. You have to make a choice for the cat to survive in an unfamiliar outside world.\n";
+			cout << "Remember, all choices and steps you make, there will be consequences.\n";
+			cout << "Choose wisely.\n";
+			backToMainMenu();
 			break;
 		}
 		case 4:
