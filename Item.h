41c3bd5728d6a40092d09dc19ee548355462db39
jx1114 <121542232+jx1114@users.noreply.github.com>
2023-06-27 18:49:27 +0800

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,46 +1,41 @@
-#include<iostream>
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
 
 using namespace std;
 
 class Item
 {
-	private:
-		//coin item
-		struct Coin
-		{
-			Coin *nextCoin;
-		};
-		Coin *coinTop;
-		//business card item
-		struct Card
-		{
-			Card *nextCard;
-		};
-		Card *cardTop;
-		//food item
-		struct Food
-		{
-			Food *nextFood;
-		};
-		Food *foodTop;
-		
-		int coinCount;
-		int cardCount;
-		int foodCount;
-		
-	public:
-		Item();
-		~Item();
-		int getCoinCount();
-		int getCardCount();
-		int getFoodCount();
-		void PushCoin();
-		void PushCard();
-		void PushFood();
-		void PopCoin();
-		void PopFood();
-		void PopCard();
-		bool isCoinEmpty();
-		bool isCardEmpty();
-		bool isFoodEmpty();
+private:
+    struct ItemNode
+    {
+        ItemNode* nextItem;
+    };
+
+    ItemNode* coinTop;
+    ItemNode* cardTop;
+    ItemNode* foodTop;
+
+    int coinCount;
+    int cardCount;
+    int foodCount;
+
+public:
+    Item();
+    ~Item();
+    int getCoinCount();
+    int getCardCount();
+    int getFoodCount();
+    void PushCoin();
+    void PushCard();
+    void PushFood();
+    void PopCoin();
+    void PopCard();
+    void PopFood();
+    bool isCoinEmpty();
+    bool isCardEmpty();
+    bool isFoodEmpty();
 };
+
+#endif
