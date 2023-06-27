ca280b9f6ee61b3eeada7c74ceb877845e6e0f99
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 22:47:24 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -13,6 +13,7 @@ Player::Player()
 	countLives = 3;
 	countHappiness = 0;
 }
+
 // Destructor to return all value to 0
 Player::~Player()
 {
@@ -21,33 +22,41 @@ Player::~Player()
 	countHappiness = 0;
 }
 
+
+
 // hold the player name after they input the name
 void Player::setName(string n)
 {
 	name = n;
 }
+
 // return the inputted value that stored in name
 string Player::getName()
 {
 	return name;
 }
 
+
+
 // hold the number of lives left of the cat
 void Player::setLives(int lives)
 {
 	countLives = lives;
 }
+
 // return the number of lives that stores in countLives
 int Player::getLives()
 {
 	return countLives;
 }
+
 // decrease number of lives when player make choice to decrease lives
 void Player::deleteLives(int num)
 {
 	//lose lives
 	countLives = countLives - num;
 }
+
 // call if number of lives less than and equal to 0
 void Player::checkLives()
 {
@@ -59,16 +68,20 @@ void Player::checkLives()
 	}
 }
 
+
+
 // hold the happiness value of cat
 void Player::setHappiness(int happy)
 {
 	countHappiness = happy;
 }
+
 // return the value of happiness
 int Player::getHappiness()
 {
 	return countHappiness;
 }
+
 // increase value of happiness if player make a choice that make the cat happy
 void Player::addHappiness(int num)
 {
