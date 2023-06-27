4760fb47b0e11db6a843558aca194c34219d2557
iuw1112 <119191251+iuw1112@users.noreply.github.com>
2023-06-27 21:43:39 +0800

    Update Item.h

--- Item.h
+++ Item.h
@@ -5,34 +5,48 @@
 
 using namespace std;
 
+// This is a class to handle the items hold by player in this game
 class Item
 {
 private:
+    // a stack that in charge of dealing with items
     struct ItemNode
     {
         ItemNode* nextItem;
     };
 
-    ItemNode* coinTop;
-    ItemNode* cardTop;
-    ItemNode* foodTop;
+    // three types of items will be handle
+    ItemNode* coinTop; // coin to give the old man
+    ItemNode* cardTop; // card that consist of photo of owner
+    ItemNode* foodTop; // to fill the stomach or give it to cat team
 
+    // hold the number of items
     int coinCount;
     int cardCount;
     int foodCount;
 
 public:
+    // Constructor
     Item();
+    // Destructor
     ~Item();
+
+    // return the number of each items
     int getCoinCount();
     int getCardCount();
     int getFoodCount();
+
+    // increase item into the stack
     void PushCoin();
     void PushCard();
     void PushFood();
+
+    // remove item from the stack
     void PopCoin();
     void PopCard();
     void PopFood();
+
+    // check if the stack is empty
     bool isCoinEmpty();
     bool isCardEmpty();
     bool isFoodEmpty();
